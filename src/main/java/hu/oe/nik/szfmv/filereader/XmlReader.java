96bfecd03e328a4b9d9e133186d5c89e7a38bd4c
Bence <hazibence1109@gmail.com>
2017-09-26 22:31:55 +0200

    XmlReader interface and WorldObject implementation

---
+++ src/main/java/hu/oe/nik/szfmv/filereader/XmlReader.java
@@ -0,0 +1,8 @@
+package hu.oe.nik.szfmv.filereader;
+
+import hu.oe.nik.szfmv.filereader.impl.WorldObject;
+import java.util.ArrayList;
+
+public interface XmlReader {
+    ArrayList<WorldObject> GetAllElements();
+}
---
+++ src/main/java/hu/oe/nik/szfmv/filereader/impl/XmlReaderImpl.java
@@ -0,0 +1,13 @@
+package hu.oe.nik.szfmv.filereader.impl;
+
+import hu.oe.nik.szfmv.filereader.XmlReader;
+
+import java.util.ArrayList;
+
+public class XmlReaderImpl implements XmlReader{
+
+    @Override
+    public ArrayList<WorldObject> GetAllElements() {
+        return null;
+    }
+}
