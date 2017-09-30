c266df50769ce408efc92419a48ce1e5d261727c
A <b@c.com>
2017-09-30 15:37:25 +0200

    Window scales based on world's dimensions
    It now:
    - has a maximum height & width
    - is scaled according to the game field's ratio to fit within maximum height & width

--- src/main/java/hu/oe/nik/szfmv/visualisation/CourseDisplay.java
+++ src/main/java/hu/oe/nik/szfmv/visualisation/CourseDisplay.java
@@ -11,6 +11,9 @@ public class CourseDisplay {
     private static final Logger logger = LogManager.getLogger();
     private JFrame frame = new JFrame("OE NIK Automated Car Project");
 
+    private static final int maxHeight = 700, maxWidth = 1100;
+    private static final double idealRatio = (double) maxWidth / (double) maxHeight;
+
     public void refreshFrame() {
         frame.invalidate();
         frame.validate();
@@ -24,8 +27,38 @@ public class CourseDisplay {
         frame.add(displayPanel);
 
         frame.validate();
-        frame.setSize(world.getWidth(), world.getHeight());
+        setFrameSize(world);
         frame.setVisible(true);
+    }
+
+    private void setFrameSize(World world) {
+        //if we need to scale the game field
+        if (world.getHeight() > maxHeight || world.getWidth() > maxWidth) {
+            scaleGameFieldToFrame(world);
+        } else {
+            frame.setSize(world.getWidth(), world.getHeight());
+        }
+    }
+
+    private void scaleGameFieldToFrame(World world) {
+        //ratio > 1: horizontal
+        //ratio < 1: vertical
+        double worldRatio = (double) world.getWidth() / (double) world.getHeight();
+        int scaledHeight, scaledWidth;
+        //worldRatio > idealRatio: we have to scale height
+        //else we have to scale width
+
+        if (worldRatio > idealRatio) {
+            scaledWidth = maxWidth;
+            scaledHeight = (int) Math.round(
+                    world.getHeight() * ((double) maxWidth / (double) world.getWidth()));
+
+        } else {
+            scaledHeight = maxHeight;
+            scaledWidth = (int) Math.round(
+                    world.getWidth() * ((double) maxHeight / (double) world.getHeight()));
+        }
+        frame.setSize(scaledWidth, scaledHeight);
     }
 
 }
