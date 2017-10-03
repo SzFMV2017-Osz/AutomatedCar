acb3e0dc2e3d55fe51abff7949cf815521de0d27
Dani Janos <dani.janos@gmail.com>
2017-10-04 01:16:33 +0200

    An ArrayList of UserInputSubscribers added to UserInputHandler class

--- src/main/java/hu/oe/nik/szfmv/inputinterface/UserInputHandler.java
+++ src/main/java/hu/oe/nik/szfmv/inputinterface/UserInputHandler.java
@@ -1,7 +1,18 @@
 package hu.oe.nik.szfmv.inputinterface;
 
+import java.util.ArrayList;
+
+/*
+maybe it should be final and singleton...
+ */
 public class UserInputHandler {
+
     private String userInput;
+    private ArrayList<UserInputSubscriber> userInputSubscribers;
+
+    public UserInputHandler() {
+        userInputSubscribers = new ArrayList<UserInputSubscriber>();
+    }
 
     public String getUserInput() {
         return userInput;
