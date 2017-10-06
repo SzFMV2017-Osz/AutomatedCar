eac949fe60de8c3d9228387060f3eaa25e69bf5a
self-driven <felisut@gmail.com>
2017-10-06 11:46:12 +0200

    Forward acceleration feature + test bench

--- src/main/java/hu/oe/nik/szfmv/automatedcar/Driver.java
+++ src/main/java/hu/oe/nik/szfmv/automatedcar/Driver.java
@@ -19,7 +19,7 @@ public class Driver extends SystemComponent {
 	private int loopCounter = 0;
 	private LocalTime elapsedTime = null;
 
-	//LoopCounter as key value
+	// LoopCounter as key value
 	private Map<Integer, Integer> gasPedalProgram = new LinkedHashMap<Integer, Integer>();
 
 	private Map<Integer, Integer> brakePedalProgram = new LinkedHashMap<Integer, Integer>();
@@ -29,13 +29,13 @@ public class Driver extends SystemComponent {
 	public Driver() {
 		super();
 		this.STARTTIME = LocalTime.now();
-		
-		//Defining some inputs from HMI
-		autoTransmissionProgram.put(10, AutoTransmissionEnum.D);
-		gasPedalProgram.put(15, 50);
-		gasPedalProgram.put(100, 100);
-		//gasPedalProgram.put(90, 0);
-		//brakePedalProgram.put(140, 50);
+
+		// Defining some inputs from HMI
+		autoTransmissionProgram.put(1, AutoTransmissionEnum.D);
+		gasPedalProgram.put(2, 100);
+		// gasPedalProgram.put(100, 100);
+		// gasPedalProgram.put(90, 0);
+		// brakePedalProgram.put(140, 50);
 	}
 
 	@Override
@@ -46,30 +46,38 @@ public class Driver extends SystemComponent {
 
 		AutoTransmissionEnum autoTransmission = autoTransmissionProgram.get(this.loopCounter);
 
-		//AutoTransmission is changed
+		// AutoTransmission is changed
 		if (autoTransmission != null) {
-			System.out.format("Elapsed: %1$tM:%1$tS:%1$tL, Loop: %2$d, Autotransmission is switched to: %3$s\n", this.STARTTIME.until(elapsedTime, ChronoUnit.MILLIS), this.loopCounter, autoTransmission);
+			System.out.format("Elapsed: %1$tM:%1$tS:%1$tL, Loop: %2$d, Autotransmission is switched to: %3$s\n",
+					this.STARTTIME.until(elapsedTime, ChronoUnit.MILLIS), this.loopCounter, autoTransmission);
 			VirtualFunctionBus.sendSignal(new Signal(SignalEnum.AUTOTRANSMISSION, autoTransmission));
 		}
-		
-		//Gas pedal is changed
+
 		Integer gasPedal = gasPedalProgram.get(this.loopCounter);
+		
+		// Gas pedal is changed
 		if (gasPedal != null) {
-			System.out.format("Elapsed: %1$tM:%1$tS:%1$tL, Loop: %2$d, Gas pedal value: %3$d\n", this.STARTTIME.until(elapsedTime, ChronoUnit.MILLIS), this.loopCounter, gasPedal);
+			System.out.format("Elapsed: %1$tM:%1$tS:%1$tL, Loop: %2$d, Gas pedal value: %3$d\n",
+					this.STARTTIME.until(elapsedTime, ChronoUnit.MILLIS), this.loopCounter, gasPedal);
 			VirtualFunctionBus.sendSignal(new Signal(SignalEnum.GASPEDAL, gasPedal));
 		}
 
-		//Brake pedal is changed
 		Integer brakePedal = brakePedalProgram.get(this.loopCounter);
+		
+		// Brake pedal is changed
 		if (brakePedal != null) {
-			System.out.format("Elapsed: %1$tM:%1$tS:%1$tL, Loop: %2$d, Brake pedal value: %3$d\n", this.STARTTIME.until(elapsedTime, ChronoUnit.MILLIS), this.loopCounter, brakePedal);
+			System.out.format("Elapsed: %1$tM:%1$tS:%1$tL, Loop: %2$d, Brake pedal value: %3$d\n",
+					this.STARTTIME.until(elapsedTime, ChronoUnit.MILLIS), this.loopCounter, brakePedal);
 			VirtualFunctionBus.sendSignal(new Signal(SignalEnum.BREAKPEDAL, brakePedal));
 		}
 	}
 
 	@Override
 	public void receiveSignal(Signal s) {
-		// TODO Auto-generated method stub
+		// Prints elapsed time to console
+		if (s.getId().equals(SignalEnum.ELAPSEDTIME)) {
+			System.out.format("Elapsed: %1$tM:%1$tS:%1$tL ", this.STARTTIME.until(LocalTime.now(), ChronoUnit.MILLIS));
+		}
 	}
 
 }
--- src/main/java/hu/oe/nik/szfmv/automatedcar/powertrainsystem/PowertrainSystem.java
+++ src/main/java/hu/oe/nik/szfmv/automatedcar/powertrainsystem/PowertrainSystem.java
@@ -1,29 +1,50 @@
 package hu.oe.nik.szfmv.automatedcar.powertrainsystem;
 
+import java.util.Arrays;
+
 import hu.oe.nik.szfmv.automatedcar.SystemComponent;
 import hu.oe.nik.szfmv.automatedcar.bus.AutoTransmissionEnum;
 import hu.oe.nik.szfmv.automatedcar.bus.Signal;
+import hu.oe.nik.szfmv.automatedcar.bus.SignalEnum;
+import hu.oe.nik.szfmv.automatedcar.bus.VirtualFunctionBus;
 
 public class PowertrainSystem extends SystemComponent {
 
-	private final double CIRCULARTRACKLENGTH = 1080;
-	private final int CENTERX = 340;
-	private final int CENTERY = 177;
-	private final double TRACKRADIUS = 172;
-	private final double REFRESHRATE = 25;
-	private final double MAXSPEED = 299.34;
-	private final double PEDALMAXVALUE = 100;
-
+	// Circular track attributes
+	private final double CIRCULAR_TRACK_LENGTH = 1080;
+	private final int CENTER_X = 340;
+	private final int CENTER_Y = 177;
+	private final double TRACK_RADIUS = 172;
 	private double positionOnTrack = 0;
-	private double currentSpeed = 0;
+
+	// Parameters needed for drawing
+	private final double REFRESH_RATE = 25;
+	private final double SPEED_CORRECTION = 3.6;
+	private final double VISUAL_CORRECTION = 15;
+
+	// Engine characteristics
+	private final double MAX_SPEED = 284.37;
+	private final double PEDAL_MAX_VALUE = 100;
+	private final double[] GEAR_RATIOS = { -1.95, 3.31, 1.95, 1.41, 1.13, 0.97, 0.82 };
+	private final double FINAL_DRIVE_RATIO = 3.88;
+	private final double[] GEAR_LEVEL_LIMITS = { -119.09, 0, 70.81, 119.09, 165.76, 206, 239.8, 300 };
+	private final double WEIGHT_OF_CAR = 1337.1;
+	private final double ENGINE_TORQUE = 151.2;
+	private final double WHEEL_DIAMETER = 0.66675;
+	private final double MIN_RPM = 600;
+	private final double MAX_RPM = 7200;
 
 	// input signals
 	private int gasPedal = 0;
 	private int breakPedal = 0;
-	private int steeringWheel = 0;
 	private AutoTransmissionEnum autoTransmission = AutoTransmissionEnum.P;
 
 	// Output signals
+	private int shiftingGrade = 1;
+	private double currentSpeed = 0;
+	private double currentRevolution = 0;
+	private double expectedRevolution = 0;
+	
 	// Only these are available trough getters
 	private int x = 0;
 	private int y = 0;
@@ -37,14 +58,60 @@ public class PowertrainSystem extends SystemComponent {
 
 	@Override
 	public void loop() {
+		
+		// Temporary variables
+		double deltaSpeed = 0;
+		double netGearRatio;
+		double torqueOnWheels;
+		double rotationalForce;
+		double acceleration;
+		
+		// Calculating shifting grade
+		switch (this.autoTransmission) {
+		case D:
+			while (this.GEAR_LEVEL_LIMITS[this.shiftingGrade + 1] < this.currentSpeed) {
+				this.shiftingGrade++;
+				VirtualFunctionBus.sendSignal(new Signal(SignalEnum.ELAPSEDTIME, 0));
+				System.out.format("Shifting grade: %d\n", this.shiftingGrade);
+			}
+
+			while (this.GEAR_LEVEL_LIMITS[this.shiftingGrade] > this.currentSpeed) {
+				this.shiftingGrade--;
+				System.out.format("Shifting grade: %d\n", this.shiftingGrade);
+			}
+			break;
+			
+		case R:
+			this.shiftingGrade = 0;
+			System.out.format("Shifting grade: %d\n", this.shiftingGrade);
+			break;
+			
+		default:
+			break;
+		}
+
+		// Calculating acceleration and delta speed
+		if (Arrays.asList(AutoTransmissionEnum.D, AutoTransmissionEnum.R).contains(this.autoTransmission)
+				&& this.gasPedal != 0) {
+			netGearRatio = this.GEAR_RATIOS[this.shiftingGrade] * this.FINAL_DRIVE_RATIO;
+			torqueOnWheels = netGearRatio * this.ENGINE_TORQUE;
+			rotationalForce = torqueOnWheels / (this.WHEEL_DIAMETER / 2);
+			acceleration = rotationalForce / this.WEIGHT_OF_CAR;
+			deltaSpeed = this.SPEED_CORRECTION * acceleration / this.REFRESH_RATE;
+		}
+
 		// Calculating velocity
-		if (autoTransmission.equals(AutoTransmissionEnum.D)) {
-			this.currentSpeed = Math.round((this.MAXSPEED * (double) this.gasPedal) / this.PEDALMAXVALUE);
+		if (this.currentSpeed < this.MAX_SPEED) {
+			this.currentSpeed += deltaSpeed;
+			if (this.currentSpeed > this.MAX_SPEED) {
+				VirtualFunctionBus.sendSignal(new Signal(SignalEnum.ELAPSEDTIME, 0));
+				this.currentSpeed = this.MAX_SPEED;
+			}
 		}
 
 		// Calculating position on the circular track
-		this.positionOnTrack = (int) Math.round(this.positionOnTrack + (this.currentSpeed / this.REFRESHRATE))
-				% this.CIRCULARTRACKLENGTH;
+		this.positionOnTrack = (int) Math.round(this.positionOnTrack + (this.currentSpeed / this.VISUAL_CORRECTION))
+				% this.CIRCULAR_TRACK_LENGTH;
 
 		this.convertToCircularTrack(this.positionOnTrack);
 	}
@@ -54,12 +121,11 @@ public class PowertrainSystem extends SystemComponent {
 		switch (s.getId()) {
 		case GASPEDAL:
 			this.gasPedal = (int) s.getData();
+			this.expectedRevolution = this.MIN_RPM
+					+ (this.MAX_RPM - this.MIN_RPM) * this.gasPedal / this.PEDAL_MAX_VALUE;
 			break;
 		case BREAKPEDAL:
 			this.breakPedal = (int) s.getData();
-			break;
-		case STEERINGWHEEL:
-			this.steeringWheel = (int) s.getData();
 			break;
 		case AUTOTRANSMISSION:
 			this.autoTransmission = (AutoTransmissionEnum) s.getData();
@@ -70,11 +136,11 @@ public class PowertrainSystem extends SystemComponent {
 	}
 
 	private void convertToCircularTrack(double positionOnTrack) {
-		this.x = CENTERX
-				+ (int) Math.round(TRACKRADIUS * Math.sin(positionOnTrack * 2 * Math.PI / CIRCULARTRACKLENGTH));
+		this.x = CENTER_X
+				+ (int) Math.round(TRACK_RADIUS * Math.sin(positionOnTrack * 2 * Math.PI / CIRCULAR_TRACK_LENGTH));
 
-		this.y = CENTERY
-				- (int) Math.round(TRACKRADIUS * Math.cos(positionOnTrack * 2 * Math.PI / CIRCULARTRACKLENGTH));
+		this.y = CENTER_Y
+				- (int) Math.round(TRACK_RADIUS * Math.cos(positionOnTrack * 2 * Math.PI / CIRCULAR_TRACK_LENGTH));
 	}
 
 	public int getX() {
