127e08d49cd31dadac59607d3c816474ec64518a
Bence Gacsalyi <bennig4@gmail.com>
2017-10-11 19:27:59 +0200

    Added properties and labels for transmission, gas and brake pedal representation, as well as basic logic for keeping the data on the HMI updated

--- src/main/java/hu/oe/nik/szfmv/visualisation/Dashboard.java
+++ src/main/java/hu/oe/nik/szfmv/visualisation/Dashboard.java
@@ -1,15 +1,21 @@
 package hu.oe.nik.szfmv.visualisation;
 
-import hu.oe.nik.szfmv.automatedcar.ISystemComponent;
+import hu.oe.nik.szfmv.automatedcar.SystemComponent;
 import hu.oe.nik.szfmv.automatedcar.bus.Signal;
 
 import javax.swing.*;
 
-public class Dashboard implements ISystemComponent {
+public class Dashboard extends SystemComponent {
     private JPanel jPanel;
+    private JLabel GasPedalLabel;
+    private JLabel BrakePedalValueLabel;
+    private JLabel TransmissionLabel;
+
+    private String gasPedalValue;
+    private String brakePedalValue;
+    private String transmissionLabelValue;
 
     public Dashboard() {
-
     }
 
     public JPanel getJPanel() {
@@ -18,11 +24,18 @@ public class Dashboard implements ISystemComponent {
 
     @Override
     public void loop() {
-
+        refreshDisplayedValues();
     }
 
     @Override
     public void receiveSignal(Signal s) {
 
+    }
+
+    private void refreshDisplayedValues()
+    {
+        GasPedalLabel.setText(gasPedalValue);
+        BrakePedalValueLabel.setText(brakePedalValue);
+        TransmissionLabel.setText(transmissionLabelValue);
     }
 }
