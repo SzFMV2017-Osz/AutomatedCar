936a07d5bdb4a8f259cb8f13f440273b0eadc89c
self-driven <felisut@gmail.com>
2017-10-12 04:15:41 +0200

    Modifying characteristics

---
+++ src/main/java/hu/oe/nik/szfmv/automatedcar/powertrainsystem/PorscheCharacteristics.java
@@ -0,0 +1,22 @@
+package hu.oe.nik.szfmv.automatedcar.powertrainsystem;
+
+public class PorscheCharacteristics extends Characteristics {
+	public PorscheCharacteristics(){
+        super(
+                "Porsche 987 Cayman S", 
+                284.37, 
+                118.4, 
+                100, 
+                new double[] { 0, 3.2935, 1.9583, 1.4069, 1.1321, 0.9726, 0.8187, -3.2935 }, 
+                3.88, 
+                new double[] { 0, 0, 69.8, 118.4, 164.4, 205.2, 239.2, 300 }, 
+                1337.1, 
+                151.2,
+                -150,
+                0.66675, 
+                600, 
+                7200, 
+                30.87
+        );
+    }
+}
