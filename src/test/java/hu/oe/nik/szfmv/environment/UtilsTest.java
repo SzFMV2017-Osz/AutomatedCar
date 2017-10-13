89971435db57ff8594b2c4e88165365878c14c99
pintergreg <pintergreg@gmail.com>
2017-10-13 16:35:42 +0200

    Add transformation matrix processor

---
+++ src/test/java/hu/oe/nik/szfmv/environment/UtilsTest.java
@@ -0,0 +1,25 @@
+package hu.oe.nik.szfmv.environment;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class UtilsTest {
+
+    @Test
+    public void testPixelConverter() {
+        assertEquals(7, Utils.convertPixelToMeter(350),0.0001d);
+    }
+
+    @Test
+    public void testMatrixToradiansConverter() {
+        assertEquals(4.71238898038469, Utils.convertMatrixToRadians(0,1,-1,0), 0.0001d);
+        assertEquals(0.7853981633974484, Utils.convertMatrixToRadians(0.7071068286895752,-0.7071068286895752,0.7071068286895752,0.7071068286895752), 0.0001d);
+    }
+
+    @Test
+    public void testRadianToDegree() {
+        assertEquals(45, Utils.radianToDegree(0.7853981633974484),0.0001d);
+    }
+
+}
