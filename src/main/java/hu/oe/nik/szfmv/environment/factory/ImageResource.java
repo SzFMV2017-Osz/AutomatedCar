8773693a238a3bc5ff5c079209f5c8aa07860eae
Kálmán Kostenszky <k.kostenszky@gmail.com>
2017-10-14 20:38:39 +0200

    ADD: new resources

--- src/main/java/hu/oe/nik/szfmv/environment/factory/ImageResource.java
+++ src/main/java/hu/oe/nik/szfmv/environment/factory/ImageResource.java
@@ -6,7 +6,12 @@ import java.util.Properties;
 public class ImageResource {
 
 	public static final String TREE_NAME = "automatedCar.tree";
+
 	public static final String WHITE_CAR_NAME = "automatedCar.car.white";
+	public static final String BLACK_CAR_NAME = "automatedCar.car.black";
+	public static final String BLUE_CAR_NAME = "automatedCar.car.blue";
+	public static final String RED_CAR_NAME = "automatedCar.car.red";
+
 	public static final String ROADSIGN_PARKING_RIGHT_NAME = "automatedCar.roadSign.parking.right";
 	public static final String ROADSIGN_STOP_NAME = "automatedCar.roadSign.stop";
 
