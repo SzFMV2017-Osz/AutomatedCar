a8d9abee7631632240b58fa4d59603dcf3bb690b
pintergreg <pintergreg@gmail.com>
2017-10-14 06:49:38 +0200

    Expand test

--- src/test/java/hu/oe/nik/szfmv/environment/XmlParserTest.java
+++ src/test/java/hu/oe/nik/szfmv/environment/XmlParserTest.java
@@ -1,5 +1,6 @@
 package hu.oe.nik.szfmv.environment;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.List;
@@ -11,8 +12,12 @@ public class XmlParserTest {
     @Test
     public void testParse() throws Exception {
         List<XmlObject> r = XmlParser.parse("test_world.xml");
-        assertEquals(r.size(), 46);
-        assertEquals(r.get(0).getX(), 1700);
+        assertEquals(46, r.size());
+        assertEquals("road_2lane_straight", r.get(0).getType());
+        assertEquals(1700, r.get(0).getX());
+        assertEquals(144, r.get(0).getY());
+        double[][] matrix = {{0, 1}, {-1, 0}};
+        Assert.assertArrayEquals(matrix, r.get(0).getMatrix());
 
     }
 }
