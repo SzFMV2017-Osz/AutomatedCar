c05479bc3307d42a5797565d888cd54748d8e2a7
Kálmán Kostenszky <k.kostenszky@gmail.com>
2017-10-14 20:39:13 +0200

    ADD: carBuilder

--- src/main/java/hu/oe/nik/szfmv/environment/object/Car.java
+++ src/main/java/hu/oe/nik/szfmv/environment/object/Car.java
@@ -1,13 +1,8 @@
 package hu.oe.nik.szfmv.environment.object;
 
-import java.util.List;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
+import hu.oe.nik.szfmv.environment.factory.ImageResource;
 import hu.oe.nik.szfmv.environment.model.MovingObject;
 import hu.oe.nik.szfmv.environment.util.ModelShape;
-import hu.oe.nik.szfmv.environment.util.Vector;
 
 /**
  * 
@@ -19,34 +14,91 @@ import hu.oe.nik.szfmv.environment.util.Vector;
  */
 public class Car extends MovingObject implements IDriveable {
 
-	public Car(int x, int y, float rotation, int width, int height, String imageFileName, int weight) {
+	public static CarBuilder builder() {
+		return new CarBuilder();
+	}
+
+	private Car(int x, int y, float rotation, int width, int height, String imageFileName, int weight) {
 		super(x, y, rotation, width, height, imageFileName, weight, ModelShape.RECTENGULAR);
 	}
 
 	@Override
 	protected void doOnCollision() {
-		log.debug("Car#doOnCollision invoked");
 		// TODO Auto-generated method stub
 	}
 
 	@Override
 	public void turn(float angle) {
-		log.debug("Car#turn invoked");
 		// TODO Auto-generated method stub
 	}
 
 	@Override
 	public void accelerate(int scale) {
-		log.debug("Car#accelerate invoked");
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void slow(int scale) {
-		log.debug("Car#slow invoked");
 		// TODO Auto-generated method stub
 
 	}
 
+	public static class CarBuilder {
+		private int x;
+		private int y;
+		private float rotation;
+		private int width = 100;
+		private int height = 100;
+		private int weight = 1000;
+		private String imageFileName;
+
+		private CarBuilder() {
+		}
+
+		public CarBuilder position(int x, int y) {
+			this.x = x;
+			this.y = y;
+			return this;
+		}
+
+		public CarBuilder rotation(float rotation) {
+			this.rotation = rotation;
+			return this;
+		}
+
+		public CarBuilder dimension(int width, int height) {
+			// TODO: implement car width, height
+			return this;
+		}
+
+		public CarBuilder weight(int weight) {
+			// TODO: implement car weight
+			return this;
+		}
+
+		public CarBuilder color(String color) {
+			String type = "";
+			switch (color.toLowerCase()) {
+			case "white":
+				type = ImageResource.WHITE_CAR_NAME;
+				break;
+			case "black":
+				type = ImageResource.BLACK_CAR_NAME;
+				break;
+			case "blue":
+				type = ImageResource.BLUE_CAR_NAME;
+				break;
+			case "red":
+				type = ImageResource.RED_CAR_NAME;
+				break;
+			}
+			imageFileName = ImageResource.getImageOf(type);
+			return this;
+		}
+
+		public Car build() {
+			return new Car(x, y, rotation, width, height, imageFileName, weight);
+		}
+	}
 }
