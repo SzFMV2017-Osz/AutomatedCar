9bdcf52cddbd14d001cc4da4dd385408dff434ae
Kálmán Kostenszky <k.kostenszky@gmail.com>
2017-10-14 18:40:25 +0200

    ADD: new records

--- src/main/java/hu/oe/nik/szfmv/environment/util/ImageNameProperty.java
+++ src/main/java/hu/oe/nik/szfmv/environment/util/ImageNameProperty.java
@@ -4,4 +4,6 @@ public class ImageNameProperty {
 
 	public static final String TREE_NAME = "automatedCar.tree";
 	public static final String WHITE_CAR_NAME = "automatedCar.car.white";
+	public static final String ROADSIGN_PARKING_RIGHT = "automatedCar.roadSign.parking.right";
+	public static final String ROADSIGN_STOP = "automatedCar.roadSign.stop";
 }
