65a763a7774e77af1aef42ac9e767cd4cb19d2b5
Akos <mezo.akos@gmail.com>
2017-10-14 15:31:12 +0200

    RoadTypes and degrees set, middle points needed to be added

--- src/main/java/hu/oe/nik/szfmv/environment/object/Road.java
+++ src/main/java/hu/oe/nik/szfmv/environment/object/Road.java
@@ -4,39 +4,65 @@ import hu.oe.nik.szfmv.environment.model.WorldObject;
 import hu.oe.nik.szfmv.environment.util.ModelShape;
 import hu.oe.nik.szfmv.environment.util.RoadType;
 
+import java.util.List;
+
 /**
  * Utat reprezentáló osztály
- * 
- * @author hunkak
  *
+ * @author hunkak
  */
 public class Road extends WorldObject {
 
-	/*
-	 * TODO: részeltesebb leíró implementáció kidolgozása, az enum kevés
-	 *
-	 * meg kell adni az út síkbeli viszonítási pontján túl az út belső viszonyítási
-	 * pontjait, pl felező vonal közepe
-	 * 
-	 */
+    /*
+     * TODO: részeltesebb leíró implementáció kidolgozása, az enum kevés
+     *
+     * meg kell adni az út síkbeli viszonítási pontján túl az út belső viszonyítási
+     * pontjait, pl felező vonal közepe
+     *
+     */
+    private RoadType roadType;
+    private int degrees;
+    private int middleX;
+    private int middleY;
+    private boolean pedestrianCrossing;
 
-	final RoadType roadType;
-	// zebra van-e rajta?
-	final boolean pedestrianCrossing;
+    public Road(int x, int y, float rotation, int width, int height, String imageFileName, boolean pedestrianCrossing) {
+        super(x, y, rotation, width, height, imageFileName, ModelShape.RECTENGULAR);
+        this.pedestrianCrossing = pedestrianCrossing;
+        this.roadType = roadType;
+        String[] explodedString = imageFileName.split("_");
+        switch (explodedString[2])
+        {
+            case "45left": this.roadType = RoadType.LEFT; this.degrees = 45; break;
+            case "45right": this.roadType = RoadType.RIGHT; this.degrees = 45; break;
+            case "6left": this.roadType = RoadType.LEFT; this.degrees = 6; break;
+            case "6right": this.roadType = RoadType.RIGHT; this.degrees = 6; break;
+            case "90left": this.roadType = RoadType.LEFT; this.degrees = 90; break;
+            case "90right": this.roadType = RoadType.RIGHT; this.degrees = 90; break;
+            case "straight": this.roadType = RoadType.STRAIGHT; break;
+            case "rotary": this.roadType = RoadType.ROTARY; break;
+            case "tjunctionleft": this.roadType = RoadType.TJUNCTIONLEFT; break;
+            case "tjunctionright": this.roadType = RoadType.TJUNCTIONRIGHT; break;
+        }
+    }
 
-	public Road(int x, int y, float rotation, int width, int height, String imageFileName, boolean pedestrianCrossing,
-			RoadType roadType) {
-		super(x, y, rotation, width, height, imageFileName, ModelShape.RECTENGULAR);
-		this.pedestrianCrossing = pedestrianCrossing;
-		this.roadType = roadType;
-	}
+    public RoadType getRoadType() {
+        return roadType;
+    }
 
-	public RoadType getRoadType() {
-		return roadType;
-	}
+    public boolean isPedestrianCrossing() {
+        return pedestrianCrossing;
+    }
 
-	public boolean isPedestrianCrossing() {
-		return pedestrianCrossing;
-	}
+    public int getDegrees() {
+        return degrees;
+    }
 
+    public int getMiddleX() {
+        return middleX;
+    }
+
+    public int getMiddleY() {
+        return middleY;
+    }
 }
