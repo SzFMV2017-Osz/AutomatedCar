292ef682fdaf16eaff56afac71dc2e405c09a5d9
A <b@c.com>
2017-10-15 17:53:28 +0200

    demo stage complete
    Team1 needs to finish proper Xml initialization.

--- src/main/java/hu/oe/nik/szfmv/Main.java
+++ src/main/java/hu/oe/nik/szfmv/Main.java
@@ -6,6 +6,7 @@ import hu.oe.nik.szfmv.environment.object.Car;
 import hu.oe.nik.szfmv.environment.xml.XmlObject;
 import hu.oe.nik.szfmv.environment.xml.XmlParser;
 import hu.oe.nik.szfmv.visualisation.CourseDisplay;
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -21,26 +22,18 @@ public class Main {
 		CourseDisplay vis = new CourseDisplay();
 
 		// create the world
-		World w = new World(5120, 3000);
+        //TODO: get this from xml
+        World w = new World(5120, 3000);
 		// create an automated car
-		Car car = Car.builder().position(500, 500).rotation(0).dimension(100, 100).weight(1000).color("black").build();
-		// add car to the world
+        Car car = Car.builder()
+                .position(500, 500)
+                .rotation(0)
+                .dimension(100, 100)
+                .weight(1000).color("black").build();
+        // add car to the world
 
-		List<XmlObject> xmlo = new ArrayList<>();
-		try {
-			xmlo = XmlParser.parse("test_world.xml");
-		}
-		catch (Exception e) {
-			logger.error(e.getMessage());
-		}
-
-		try {
-			for (XmlObject item : xmlo) {
-                w.addObjectToWorld(new WorldObject(item.getX(), item.getY(), -(float) ((double) item.getRotation() / 180 * Math.PI), 10, 10, item.getType().getXmlName() + ".png", null));
-            }
-		}
-		catch (Exception e) {
-		}
+        //!ONLY FOR TESTING!
+        testInitFromXml(w);
 
 
 		w.addObjectToWorld(car);
@@ -58,4 +51,30 @@ public class Main {
 			}
 		}
 	}
+
+    //!ONLY FOR TESTING!
+    private static void testInitFromXml(World w) {
+        logger.log(Level.WARN, "@Team1: fix this, WorldObject initialization method is only for testing");
+        List<XmlObject> xmlo = new ArrayList<>();
+        try {
+            xmlo = XmlParser.parse("test_world.xml");
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
+
+        try {
+            for (XmlObject item : xmlo) {
+                w.addObjectToWorld(
+                        new WorldObject(
+                                item.getX(),
+                                item.getY(),
+                                -(float) ((double) item.getRotation() / 180 * Math.PI),
+                                10,
+                                10,
+                                item.getType().getXmlName() + ".png",
+                                null));
+            }
+        } catch (Exception e) {
+        }
+    }
 }
