ab2ee3496b1518af008879892c37140c4b2689cf
Dani Janos <dani.janos@gmail.com>
2017-11-06 23:24:39 +0100

    Change: new testbed for reconstituted WorldObject for issue #166

--- src/test/java/hu/oe/nik/szfmv/collisiondetection/CollisionDetectionTest.java
+++ src/test/java/hu/oe/nik/szfmv/collisiondetection/CollisionDetectionTest.java
@@ -24,46 +24,51 @@ public class CollisionDetectionTest {
         );
 
         this.automatedCarMock = new AutomatedCarMock(
-                100, 100, 0F,"car_2_white.png", ModelShape.RECTENGULAR
+                100, 100, 0F, "car_2_white.png", ModelShape.RECTENGULAR
         );
     }
 
     @Test
     public void testCollision() throws Exception {
-        assertEquals(this.treeMock.isCollided(),false);
+        assertEquals(this.treeMock.isCollided(), false);
         this.treeMock.onCollision();
-        assertEquals(this.treeMock.isCollided(),true);
+        assertEquals(this.treeMock.isCollided(), true);
     }
 
     @Test
-    public void testMoveAndCollide() throws Exception{
-        this.automatedCarMock.move(10,10);
-        assertEquals(this.automatedCarMock.getX(),110,0.1);
-        assertEquals(this.automatedCarMock.getY(),110,0.1);
+    public void testMoveAndCollide() throws Exception {
+
+        Vector2D currentPossition = this.automatedCarMock.getPosition();
+        this.automatedCarMock.setPosition(currentPossition.add(new Vector2D(100, 100)));
 
         boolean areTheyIntersecting = this.treeMock.isIntersects(this.automatedCarMock);
         assertEquals(areTheyIntersecting, false);
 
-        this.automatedCarMock.move(-110,-110);
-        assertEquals(this.automatedCarMock.getX(),0,0.1);
-        assertEquals(this.automatedCarMock.getY(),0,0.1);
-
-        areTheyIntersecting = this.treeMock.isIntersects(this.automatedCarMock);
-        assertEquals(areTheyIntersecting, true);
+        this.automatedCarMock.setPosition(this.automatedCarMock.getPosition().add(new Vector2D(-100, -100)));
+        assertEquals(this.treeMock.isIntersects(this.automatedCarMock), true);
     }
 
     class TreeMock extends CollidableObject {
 
+        @SuppressWarnings("WeakerAccess")
         public TreeMock(int x, int y, float rotation, String imageFileName, int weight, ModelShape shape) {
             super(x, y, rotation, imageFileName, weight, shape);
         }
 
-        boolean isIntersects(WorldObject worldObject){
-
-            Rectangle rectangleFromThis = new Rectangle((int)this.getX(), (int)this.getY(), this.getWidth(), this.getHeight());
-            Rectangle rectangleFromWorldObject = new Rectangle((int)worldObject.getX(), (int)worldObject.getY(), worldObject.getWidth(), worldObject.getHeight());
-
-            return rectangleFromThis.intersects(rectangleFromWorldObject);
+        /**
+         * Determines whether or not this <code>CollidableObject</code> and the specified
+         * <code>WorldObject</code> intersect. Two objects intersect if
+         * their intersection is nonempty.
+         *
+         * @param worldObject the specified <code>WorldObject</code>
+         * @return <code>true</code> if the specified <code>WorldObject</code>
+         * and this <code>CollidableObject</code> intersect;
+         * <code>false</code> otherwise.
+         */
+        boolean isIntersects(WorldObject worldObject) {
+            Rectangle rectangleFromThisObject = new Rectangle((int) this.getX(), (int) this.getY(), this.getWidth(), this.getHeight());
+            Rectangle rectangleFromWorldObject = new Rectangle((int) worldObject.getX(), (int) worldObject.getY(), worldObject.getWidth(), worldObject.getHeight());
+            return rectangleFromThisObject.intersects(rectangleFromWorldObject);
         }
 
         @Override
@@ -74,13 +79,9 @@ public class CollisionDetectionTest {
 
     class AutomatedCarMock extends WorldObject {
 
+        @SuppressWarnings("WeakerAccess")
         public AutomatedCarMock(int x, int y, float rotation, String imageName, ModelShape shape) {
             super(x, y, rotation, imageName, shape);
         }
-
-        public void move(int x, int y){
-            this.setPosition(new Vector2D(x,y));
-        }
-
     }
 }
